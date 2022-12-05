c3390ec202f8c740411d26160da74b9b1d6a747b
Alexandr <sashapopovkin13@yandex.ru>
2022-12-05 22:46:18 +0300

    second version

--- main.py
+++ main.py
@@ -1,16 +1,80 @@
-# This is a sample Python script.
+def _pre_to_inf(symb, text):
+    if symb in ['+', '-', '*', '/']:
+        left = text.pop ()
+        right = text.pop ()
+        text.append ('(%s %s %s)' % (left, symb, right))
+    elif symb == '!':
+       c = text.pop ()
+       text.append('%s!' % (op) )
+    elif symb in ['sin', 'cos', 'tan']:
+       c = text.pop ()
+       text.append('%s(%s)' % (symb,c))
+    else:
+       text.append(symb)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def pre_to_inf(a_list):
+    text = []
+    a_list.reverse ()
+    for i in a_list:
+        _pre_to_inf(i, text)
+    return text.pop()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def _post_to_inf(symb,text):
+    if symb in ['+', '-', '*', '/']:
+        left = text.pop ()
+        right = text.pop ()
+        text.append ('(%s %s %s)' % (left, symb, right))
+    elif symb == '!':
+       c = text.pop ()
+       text.append('%s!' % c )
+    elif symb in ['sin', 'cos', 'tan']:
+       c = text.pop ()
+       text.append('%s(%s)' % (symb,c))
+    else:
+       text.append(symb)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def post_to_inf(a_list):
+    text = []
+    for i in a_list:
+        _post_to_inf(i, text)
+    return text.pop()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def main():
+
+    ch = 1
+    while ch != 0 :
+        print('\n---------------------------------\n'
+              '1. Ввести выражение в инфиксной форме\n'
+              '2. Ввести выражение в префиксной форме\n'
+              '3. Ввести выражение в постфиксной форме\n'
+              '4. Отображение результата\n'
+              '0. Выход\n'
+              '---------------------------------\n')
+        try:
+            ch = int(input('Выберите пункт меню: '))
+        except:
+            print('Введено некорректное значение\n')
+        if ((ch>11) or (ch<0)):
+            print('Введите корректное значение\n')
+        else:
+            if ch == 1:
+                a = input('Введите выражение в инфиксной форме: ')
+
+            elif ch == 2:
+                a = input('Введите выражение в префиксной форме (вводить нужно через пробел): ')
+                a_list = a.split(' ')
+
+                a = pre_to_inf(a_list)
+
+            elif ch == 3:
+                a = input('Введите выражение в постфиксной форме: ')
+                a_list = a.split(' ')
+
+                a = post_to_inf(a_list)
+
+            elif ch == 4:
+                result = eval(a)
+                print(result)
+main()
