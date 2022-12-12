83869ab8f7835f223c74e193b6270f715bf94d83
Alexandr <sashapopovkin13@yandex.ru>
2022-12-12 22:36:01 +0300

    fixed

--- main.py
+++ main.py
@@ -2,7 +2,17 @@ def _pre_to_inf(symb, text):
     if symb in ['+', '-', '*', '/', '**']:
         left = text.pop ()
         right = text.pop ()
-        text.append ('(%s %s %s)' % (left, symb, right))
+        if symb == "+":
+            ans = int(left) + int(right)
+        elif symb == "-":
+            ans = int(left) - int(right)
+        elif symb == "/":
+            ans = int(left) / int(right)
+        elif symb == "*":
+            ans = int(left) * int(right)
+        elif symb == "**":
+            ans = int(left) ** int(right)
+        text.append(ans)
     else:
        text.append(symb)
 
@@ -19,7 +29,17 @@ def _post_to_inf(symb,text):
     if symb in ['+', '-', '*', '/', '**']:
         left = text.pop ()
         right = text.pop ()
-        text.append ('(%s %s %s)' % (right, symb, left))
+        if symb == "+":
+            ans = int(left) + int(right)
+        elif symb == "-":
+            ans = int(left) - int(right)
+        elif symb == "/":
+            ans = int(right) / int(left)
+        elif symb == "*":
+            ans = int(left) * int(right)
+        elif symb == "**":
+            ans = int(right) ** int(left)
+        text.append(ans)
     else:
        text.append(symb)
 
@@ -30,6 +50,59 @@ def post_to_inf(a_list):
         _post_to_inf(i, text)
 
     return text[0]
+
+def ev(a_list):
+    ans = 0
+    i = 0
+    while len(a_list)!=1:
+
+            if len(a_list)-1 == i: i = 0
+            elif (a_list[i] == '(') and (a_list[i+1]!= '(') and (a_list[i+2]!=')') and (a_list[i+3]!='('):
+                if a_list[i+2] == '+':
+                    ans = int(a_list[i+1]) + int(a_list[i+3])
+                elif a_list[i+2] == '*':
+                    ans = int(a_list[i+1]) * int(a_list[i+3])
+                elif a_list[i+2] == '/':
+                    ans = int(a_list[i+1]) / int(a_list[i+3])
+                elif a_list[i+2] == '**':
+                    ans = int(a_list[i+1]) ** int(a_list[i+3])
+                elif a_list[i+2] == '-':
+                    ans = int(a_list[i+1]) - int(a_list[i+3])
+                a_list[i] = ans
+
+                a_list.remove(a_list[i + 1])
+                a_list.remove(a_list[i + 1])
+                a_list.remove(a_list[i + 1])
+
+
+            elif (a_list[i+1]==')') and (a_list[i+1]):
+                a_list.remove(a_list[i+1])
+
+
+            elif (str(a_list[i])[0] in "123456789") and (a_list[i+2]!='(') and (str(a_list[i]) not in '+-*/**'):
+                if a_list[i+1] == '+':
+                    ans = int(a_list[i]) + int(a_list[i+2])
+                elif a_list[i+1] == '*':
+                    ans = int(a_list[i]) * int(a_list[i+2])
+                elif a_list[i+1] == '/':
+                    ans = int(int(a_list[i]) / int(a_list[i+2]))
+                elif a_list[i+1] == '**':
+                    ans = int(a_list[i]) ** int(a_list[i+2])
+                elif a_list[i+1] == '-':
+                    ans = int(a_list[i]) - int(a_list[i+2])
+                a_list[i] = ans
+                a_list.remove(a_list[i+1])
+                a_list.remove(a_list[i+1])
+            elif (a_list[i] == '(' and a_list[i+1] == '('):
+                a_list.remove(a_list[i])
+            elif not (a_list[i+1]):
+                i = 0
+
+            else:
+                i=i+1
+
+
+    return(a_list[0])
 
 def main():
 
@@ -52,13 +125,14 @@ def main():
             if ch == 1:
                 a = input('Введите выражение в инфиксной форме: ')
                 a = a.replace("^", "**")
+                a = a.split(' ')
+                a = ev(a)
                 print(a)
 
             elif ch == 2:
                 a = input('Введите выражение в префиксной форме (вводить нужно через пробел): ')
                 a = a.replace("^", "**")
                 a_list = a.split(' ')
-
                 a = pre_to_inf(a_list)
 
             elif ch == 3:
@@ -67,9 +141,6 @@ def main():
                 a_list = a.split(' ')
                 a = post_to_inf(a_list)
 
-
-
             elif ch == 4:
-                result = eval(a)
-                print(result)
+                print(a)
 main()
